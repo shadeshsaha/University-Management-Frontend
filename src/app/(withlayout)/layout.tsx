8f9bd0cbb77a946327d58c4a4b80c8485cd65a95
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-10-09 18:53:04 +0600

    Customized Login page and Create reusable breadCrumb component

--- src/app/(withlayout)/layout.tsx
+++ src/app/(withlayout)/layout.tsx
@@ -1,5 +1,5 @@
-import Contents from "@/components/Contents";
-import SideBar from "@/components/Sidebar";
+import Contents from "@/components/ui/Contents";
+import SideBar from "@/components/ui/Sidebar";
 import { Layout } from "antd";
 
 // ei layout ta (withlayout) er moddhe jeshob page thakbe, segulo k receive korbe
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,20 +20,36 @@ const LoginPage = () => {
   };
 
   return (
-    <Row>
-      <Col sm={12} md={16} lg={16}>
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Col sm={12} md={16} lg={10}>
         <Image src={loginImage} width={500} alt="login image" />
       </Col>
 
       <Col sm={12} md={8} lg={8}>
-        <h1>First login your account</h1>
+        <h1
+          style={{
+            margin: "15px 0px",
+          }}
+        >
+          First login your account
+        </h1>
 
         <div>
           <Form submitHandler={onSubmit}>
             <div>
               <FormInput name="id" type="text" size="large" label="User ID" />
             </div>
-            <div>
+            <div
+              style={{
+                margin: "15px 0px",
+              }}
+            >
               <FormInput
                 name="password"
                 type="password"
