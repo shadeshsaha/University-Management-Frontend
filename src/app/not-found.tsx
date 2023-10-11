ca076e7c5d683670ee10a93fdfed66404218fece
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-10-11 18:44:15 +0600

    Updated 404 Page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,34 @@
+// import { Row } from "antd";
+import { Button, Result, Row } from "antd";
+
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404!!! Page not found!</h1>
-    </div>
+    // 1st
+    // <Row
+    //   justify="center"
+    //   align="middle"
+    //   style={{
+    //     height: "100vh",
+    //   }}
+    // >
+    //   <h1>404!!! Page Not Found!</h1>
+    // </Row>
+
+    // 2nd
+    <Row
+      justify={"center"}
+      align={"middle"}
+      style={{
+        height: "100vh",
+      }}
+    >
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={<Button type="primary">Back Home</Button>}
+      />
+    </Row>
   );
 };
 
