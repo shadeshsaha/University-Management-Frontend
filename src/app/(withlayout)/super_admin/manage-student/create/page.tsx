e86dae4b7b0a99eda5d5c72fbac1f47466648af4
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-10-12 19:41:17 +0600

    Created reusable Stepper Form to create student form

--- src/app/(withlayout)/super_admin/manage-student/create/page.tsx
+++ src/app/(withlayout)/super_admin/manage-student/create/page.tsx
@@ -1,7 +1,30 @@
+import StepperForm from "@/components/StepperForm/StepperForm";
+import StudentInfo from "@/components/StudentForms/StudentInfo";
+
 const CreateStudentPage = () => {
+  const steps = [
+    {
+      title: "Student Information",
+      content: <StudentInfo />,
+    },
+    {
+      title: "Basic Information",
+      content: "Second-content",
+    },
+    {
+      title: "Guardian Information",
+      content: "Last-content",
+    },
+    {
+      title: "Local Guardian Information",
+      content: "Last-content",
+    },
+  ];
+
   return (
     <div>
       <h1>Create Student</h1>
+      <StepperForm steps={steps} />
     </div>
   );
 };
