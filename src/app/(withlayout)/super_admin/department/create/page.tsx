6c79a0bd0a7f7382147b1654b71cae1ef7c9afb9
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-10-13 19:14:00 +0600

    Added Create Department Form in Super-Admin

--- src/app/(withlayout)/super_admin/department/create/page.tsx
+++ src/app/(withlayout)/super_admin/department/create/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { Button, Col, Row } from "antd";
 
 const CreateDepartmentPage = () => {
+  const onSubmit = async (data: any) => {
+    try {
+      console.log(data);
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+
   const base = "super_admin";
+
   return (
     <div>
       <UMBreadCrumb
@@ -11,6 +25,17 @@ const CreateDepartmentPage = () => {
         ]}
       />
       <h1>Create Department</h1>
+
+      <Form submitHandler={onSubmit}>
+        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+          <Col span={8} style={{ margin: "10px 0" }}>
+            <FormInput name="title" label="Title" />
+          </Col>
+        </Row>
+        <Button type="primary" htmlType="submit">
+          add
+        </Button>
+      </Form>
     </div>
   );
 };
