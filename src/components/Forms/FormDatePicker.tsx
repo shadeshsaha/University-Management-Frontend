b35de52e7f4368bc373aa86b71da91660233070d
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-10-16 06:19:08 +0600

    Resolved Date Picker Issues

--- src/components/Forms/FormDatePicker.tsx
+++ src/components/Forms/FormDatePicker.tsx
@@ -35,7 +35,7 @@ const FormDatePicker = ({
         control={control}
         render={({ field }) => (
           <DatePicker
-            value={dayjs(field.value) || ""}
+            defaultValue={dayjs(field.value) || ""}
             size={size}
             onChange={handleOnChange}
             style={{ width: "100%" }}
